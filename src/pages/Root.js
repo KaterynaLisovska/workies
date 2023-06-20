7c798874b7dbd2619bc02f6ccaaf0ee940ee4b8a
KateSmaug <katya.polischuk@gmail.com>
2023-06-20 14:03:17 +0300

    Root Page Updated

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,5 +1,13 @@
-const RootLayout = () => {
-    return;
+import { Outlet } from "react-router-dom";
+
+function RootLayout() {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default RootLayout;
