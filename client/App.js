bfdef0f06082bd1269f8c2c497caa78aa081afe7
KateSmaug <katya.polischuk@gmail.com>
2023-06-27 11:10:53 +0300

    Change project structure

--- client/App.js
+++ client/App.js
@@ -2,11 +2,11 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
-import SignUpPage from "./pages/SignUp";
-import LoginPage from "./pages/Login";
-import ErrorPage from "./pages/Error";
-import RootLayout from "./pages/Root";
-import HomePage from "./pages/Home";
+import SignUpPage from "./src/pages/SignUp";
+import LoginPage from "./src/pages/Login";
+import ErrorPage from "./src/pages/Error";
+import RootLayout from "./src/pages/Root";
+import HomePage from "./src/pages/Home";
 
 const router = createBrowserRouter([
   {
