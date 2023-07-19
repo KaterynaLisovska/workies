f78ccec660f81ea638bf170b74ab2ed8bc63fb8d
KateSmaug <katya.polischuk@gmail.com>
2023-07-19 15:38:34 +0300

    "Removed App.css & Renamed form's style"

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,11 @@ import { SlCard } from "@shoelace-style/shoelace/dist/react";
 
 import MainNavigation from "./MainNavigation";
 
-import "../App.css";
-
 const HomePage = () => {
   return (
     <>
       <SlCard className="form">
-        <form className="text_position">
+        <form className="centered">
           <MainNavigation />
           <h1>Home Page</h1>
         </form>
